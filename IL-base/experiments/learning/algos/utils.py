15a62a6cfad2768e91d1dde38934e056a5b55eb2
karush17 <karushsuri@gmail.com>
2021-02-22 00:35:47 -0500

    add color from camer image

--- IL-base/experiments/learning/algos/utils.py
+++ IL-base/experiments/learning/algos/utils.py
@@ -131,7 +131,7 @@ def record_video(args, policy, eval_env, seed, shared_constants, filename):
 	while not done:
 		action = policy.select_action(np.array(state))
 		state, reward, done, _ = eval_env.step(action)
-		im = Image.fromarray(state[0,:,:,0:2])
+		im = Image.fromarray(state[0,:,:,0:3])
 		images.append(im.convert('P'))
 	images[0].save(filename+'/'+str(time.time())+'-.gif', save_all=True, append_images=images[1:], optimize=False, duration=20, loop=0)
 
