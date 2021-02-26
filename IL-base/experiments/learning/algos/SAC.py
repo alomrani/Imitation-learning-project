abd2f58ef0df3208a26ea237e50b9c7a1abf01f7
karush17 <karushsuri@gmail.com>
2021-02-26 14:44:16 -0500

    add conv encoder for sac

--- IL-base/experiments/learning/algos/SAC.py
+++ IL-base/experiments/learning/algos/SAC.py
@@ -10,79 +10,127 @@ LOG_SIG_MAX = 2
 LOG_SIG_MIN = -5
 epsilon = 1e-6
 hidden_size = 1024
+encoder_size = 50
 
 def weights_init_(m):
     if isinstance(m, nn.Linear):
         torch.nn.init.xavier_uniform_(m.weight, gain=1)
         torch.nn.init.constant_(m.bias, 0)
 
-
-class ActorCNN(nn.Module):
-	def __init__(self, state_dim, action_dim, max_action):
-		super(ActorCNN, self).__init__()
+class Encoder(nn.Module):
+    def __init__(elf, state_dim):
+        super(Encoder, self).__init__()
 		self.state_dim = state_dim
-		self.action_dim = action_dim
+        self.detach = True
 		self.features = nn.Sequential(
-			nn.Conv2d(state_dim, 32, kernel_size=8, stride=4, padding=0),
-			nn.ReLU(),
-			nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-			nn.ReLU(),
-			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-			nn.ReLU()
+			nn.Conv2d(state_dim, 32, kernel_size=3, stride=2),
+            nn.ReLU(),
+			nn.Conv2d(32, 32, kernel_size=3, stride=1),
+            nn.ReLU(),
+			nn.Conv2d(32, 32, kernel_size=3, stride=1),
+            nn.ReLU(),
+			nn.Conv2d(32, 32, kernel_size=3, stride=1)
+            nn.ReLU(),
 		)
 		self.fc = nn.Sequential(
-			nn.Linear(512, 256),
-			nn.ReLU(),
-			nn.Linear(256, action_dim)
+			nn.Linear(32*35*35, encoder_size),
+			nn.LayerNorm(encoder_size)
 		)
-		self.max_action = max_action
 
-	def forward(self, x):
-		x = self.features(x.transpose(1,3))
+    def forward(self, x):
+        x = self.features(x.transpose(1,3))
 		x = x.view(x.size(0), -1)
-		x = self.fc(x)
-		return self.max_action * torch.tanh(x)
-    
+        if self.detach:
+            x.detach()
+        x = self.fc(x)
+        return x
+
+    def copy_conv_weights_from(self, source):
+        """Tie convolutional layers"""
+        for i in range(self.num_layers):
+            self.convs[i].weight = source.convs[i].weight
+            self.convs[i].bias = source.convs[i].bias
+
+
+class ActorCNN(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(ActorCNN, self).__init__()
+        self.encoder = Encoder(num_inputs)
+        self.linear1 = nn.Linear(encoder_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+
+        self.mean_linear = nn.Linear(hidden_size, num_actions)
+        self.log_std_linear = nn.Linear(hidden_size, num_actions)
+
+        self.apply(weights_init_)
+
+        # action rescaling
+        # if action_space is None:
+        #     self.action_scale = torch.tensor(1.)
+        #     self.action_bias = torch.tensor(0.)
+        # else:
+        self.action_scale = torch.tensor(
+            1.)
+        self.action_bias = torch.tensor(
+            1.)
+
+    def forward(self, state):
+        x = self.encoder(state)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
+        return mean, log_std
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + epsilon)
+        log_prob = log_prob.sum(1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        return action, log_prob, mean
+
+    def to(self, device):
+        self.action_scale = self.action_scale.to(device)
+        self.action_bias = self.action_bias.to(device)
+        return super(Actor, self).to(device)
 
 
 class CriticCNN(nn.Module):
 	def __init__(self, state_dim, action_dim):
 		super(CriticCNN, self).__init__()
-		self.state_dim = state_dim
-		self.action_dim = action_dim
-		self.features = nn.Sequential(
-			nn.Conv2d(state_dim, 32, kernel_size=8, stride=4, padding=0),
-			nn.ReLU(),
-			nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-			nn.ReLU(),
-			nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-			nn.ReLU()
-		)
-		self.fc1 = nn.Sequential(
-			nn.Linear(512+self.action_dim, 256),
-			nn.ReLU(),
-			nn.Linear(256, 1)
-		)
-		self.fc2 = nn.Sequential(
-			nn.Linear(512+self.action_dim, 256),
-			nn.ReLU(),
-			nn.Linear(256, 1)
-		)
-        
-	def forward(self, x, action):
-		x = self.features(x.transpose(1,3))
-		x = x.view(x.size(0), -1)
-		sa = torch.cat([x, action], axis=1)
-		q1 = self.fc1(sa)
-		q2 = self.fc2(sa)
+        self.encoder = Encoder(num_inputs)
+		# Q1 architecture
+		self.l1 = nn.Linear(encoder_size + action_dim, hidden_size)
+		self.l2 = nn.Linear(hidden_size, hidden_size)
+		self.l3 = nn.Linear(hidden_size, 1)
+
+		# Q2 architecture
+		self.l4 = nn.Linear(encoder + action_dim, hidden_size)
+		self.l5 = nn.Linear(hidden_size, hidden_size)
+		self.l6 = nn.Linear(hidden_size, 1)
+
+
+	def forward(self, state, action):
+        enc_state = self.encoder(state)
+		sa = torch.cat([enc_state, action], 1)
+
+		q1 = F.relu(self.l1(sa))
+		q1 = F.relu(self.l2(q1))
+		q1 = self.l3(q1)
+
+		q2 = F.relu(self.l4(sa))
+		q2 = F.relu(self.l5(q2))
+		q2 = self.l6(q2)
 		return q1, q2
 
-	def Q1(self, x, action):
-		x = self.features(x.transpose(1,3))
-		x = x.view(x.size(0), -1)
-		sa = torch.cat([x, action], axis=1)
-		q1 = self.fc1(sa)
-		return q1
 
 class Actor(nn.Module):
     def __init__(self, num_inputs, num_actions):
@@ -191,9 +239,10 @@ class SAC(object):
         if args.obs==ObservationType.KIN:
             self.actor = Actor(state_dim, action_dim).to(self.device)
             self.critic = Critic(state_dim, action_dim).to(device=self.device)
-        # else:
-        # 	self.actor = ActorCNN(state_dim, action_dim).to(device)
-        # 	self.critic = CriticCNN(state_dim, action_dim).to(device)
+        else:
+        	self.actor = ActorCNN(state_dim, action_dim).to(device)
+        	self.critic = CriticCNN(state_dim, action_dim).to(device)
+            self.actor.encoder.copy_conv_weights_from(self.critic.encoder)
 
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.args.lr)
 
