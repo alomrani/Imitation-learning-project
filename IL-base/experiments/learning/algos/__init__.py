e1da996cef5ab97ab48a70a1f91d3ace4030a0d4
karush17 <karushsuri@gmail.com>
2021-03-04 19:12:01 -0500

    added experts

--- IL-base/experiments/learning/algos/__init__.py
+++ IL-base/experiments/learning/algos/__init__.py
@@ -3,5 +3,6 @@ from algos.DDPG import *
 from algos.SAC import *
 from algos.SAC_IM import *
 from algos.IL import *
+from algos.CQL import *
 from algos.utils import *
 from algos.variational import *
